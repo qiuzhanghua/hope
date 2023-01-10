e2a79790aa63f6a0d12ba3fd5d07473eaf087f39
qiuzhanghua <qiuzhanghua@icloud.com>
2023-01-10 20:22:21 +0800

    add stitches

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import { hope, Box, Grid } from "@hope-ui/solid";
-
+import { styled } from "solid-stitches";
 export default function App () {
+  const Button = styled("button", {
+    backgroundColor: "gainsboro",
+    borderRadius: "9999px",
+    fontSize: "16px",
+    border: "0",
+    color: "red",
+    margin: "1em",
+  });
+
   return (
     <>
+      <Button>Stitch</Button>
       <Box m="$2">Tomato</Box>
       <Grid gap={2} autoFlow="row dense">
         Grid
